7e2f53d47b0cf5835212d5e5c04c76f6cf6a4516
蔡耀斌 <112918606+tsaiyaobin@users.noreply.github.com>
2025-04-14 15:07:21 +0800

    Update 0325_5.py

--- week06/0325_5.py
+++ week06/0325_5.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-# bin 是 x 的每一數值稱為 bin
-# np.histogram() 計算直方圖
-# cv2.calcHist(images, channels, mask, histSize, ranges) 計算直方圖
-# channels: gray[0] ; color image,B:[0] G:[1] R:[2]
-# 直方圖可以用來做影像匹配
+
 img=cv2.imread("img3.jpg",0)
 img=cv2.resize(img,(300,300))
-hist=cv2.calcHist([img],[0],None,[256],[0,256])
 
+# cv2.calcHist(images, channels, mask, histSize, ranges) 計算直方圖
+# channels : gray[0] ; color image,B:[0] G:[1] R:[2]
+# histSize : BIN count
+# ranges : 像素值範圍, Normally, it is [0,256]
+
+hist=cv2.calcHist( [img] , [0] , None , [256] , [0,256] )
 
 plt.hist(img.ravel(),256)
 plt.show()
